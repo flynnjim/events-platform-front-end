188cad441aa272a538644b2c4638c6d87e57a106
flynnjim <flynnlomas@hotmail.co.uk>
2025-03-27 17:37:35 +0000

    added sticky header and nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,14 @@ import FullMap from "./components/FullMap/FullMap";
 import Login from "./components/Login/Login";
 import AddEvent from "./components/AddEvent/AddEvent";
 import AmendEvent from "./components/AmendEvent/AmendEvent";
-
 import { Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <>
-      <div>
-        <Header />
-        <Navigation />
-
+      <Header />
+      <Navigation />
+      <div className="mainContent">
         <Routes>
           <Route path="/" element={<DisplayAllEvents />} />
           <Route path="/events/:category" element={<DisplayAllEvents />} />
